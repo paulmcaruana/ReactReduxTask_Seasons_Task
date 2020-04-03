c935a66ef65964655cfb7299cea013b0afd1f198
Paul <paul@beambient.co.uk>
2020-04-03 16:35:51 +0100

    addition of icons & logic to display right season and text based on geolocation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-class App extends React.Component {  //changed from a functional app component in last commit
-    constructor(props) {
-        super(props); //this must be done every time we define a constructor function inside a class
+class App extends React.Component {                     //changed from a functional app component in last commit
 
-        //THIS IS THE ONLY TIME we do direct assignment to 
-        //this.state
-        this.state = { lat: null, errorMessage: '' }; //javascript object assigned to this.state
+    state = { lat: null, errorMessage: ''};                 //equivalent to the whole construtor previously done - babel builds it to the full equivalent
 
+    componentDidMount (){                                  //removed from the constructor and refactored 
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // we called setState!!
-                this.setState({ lat: position.coords.latitude});
-
-                //we did not 
-                //this.state.lat = position.coords.latitude
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message});
-            }
+            (position) => this.setState({ lat: position.coords.latitude}),
+            (err) => this.setState({ errorMessage: err.message})
         );
+
     }
+
     // React says we have to define render!!
     render() {
         if (this.state.errorMessage && !this.state.lat){
@@ -29,7 +21,7 @@ class App extends React.Component {  //changed from a functional app component i
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat}/>;
         }
 
         return <div>Loading!</div>;
